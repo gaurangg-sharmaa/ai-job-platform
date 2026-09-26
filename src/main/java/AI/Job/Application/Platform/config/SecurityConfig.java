1f3ed01ae38978c8c512a3772e096027838b1427
Gaurang Sharma <157728638+gaurang003@users.noreply.github.com>
2026-09-26 20:46:10 +0530

    Indentation of code.

--- src/main/java/AI/Job/Application/Platform/config/SecurityConfig.java
+++ src/main/java/AI/Job/Application/Platform/config/SecurityConfig.java
@@ -10,9 +10,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-
-
+        
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
